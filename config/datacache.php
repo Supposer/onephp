98471b5568ac5134185e56ded81002012652298a
huzhiliang <404355382@qq.com>
2014-03-29 14:30:46 +0800

    datacache config
    datacache config

---
+++ config/datacache.php
@@ -0,0 +1,16 @@
+<?php
+/** 
+* datacache.php
+* @copyright (C) 2012-2013 
+* @license 
+* @lastmodify 2013-4-14
+* @author liang
+*/
+defined('IN_ONE') or exit('Access Denied');
+return array(
+	'mode' => '',//缓存模式
+	'pre' =>'pre',//缓存前缀
+	'ttl' => '900',//缓存时间(秒)
+	'server' => '',//服务器
+	'port' => ''//端口
+);
